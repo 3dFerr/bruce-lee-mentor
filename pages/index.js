f6b66faebffda7eb8ec0e5a17db7cff1f763a10b
3dFerr <pauloferrti@gmail.com>
2023-05-06 11:04:32 -0300

    c07 - insert tittle and subtittle

--- pages/index.js
+++ pages/index.js
@@ -19,10 +19,14 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Bruce Lee Mentor</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>
+              Escreva um tópico que você gostaria que Donald Trump explicasse
+              para você (ex: fissão nuclear, a filosofia das artes maciais, o
+              significado da vida).
+            </h2>
           </div>
         </div>
         <div className="prompt-container">
